2294fd3ac3edfb1ff0c65d89cd11c1ae0a69f63d
Michael Hartl <michael@michaelhartl.com>
2012-03-21 12:52:29 -0700

    Add a test for showing 2nd page of users

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -42,6 +42,16 @@ describe "User pages" do
           page.should_not have_selector('li', text: user.name)
         end
       end
+
+      describe "showing the second page" do
+        before { visit users_path(page: 2) }
+
+        it "should list the second page of users" do
+          second_page.each do |user|
+            page.should have_selector('li', text: user.name)
+          end
+        end
+      end
       
       it { should_not have_link('delete') }
 
@@ -53,9 +63,11 @@ describe "User pages" do
         end
 
         it { should have_link('delete', href: user_path(User.first)) }
+        
         it "should be able to delete another user" do
           expect { click_link('delete') }.to change(User, :count).by(-1)
         end
+
         it { should_not have_link('delete', href: user_path(admin)) }
       end   
     end
