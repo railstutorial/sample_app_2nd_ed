f66ba2b8bf0ab00e2be46d6bdcd1037a17167a5c
Michael Hartl <michael@michaelhartl.com>
2012-03-30 20:25:05 -0700

    Add test for Micropost user_id

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.2'
+gem 'rails', '3.2.3'
 gem 'bootstrap-sass', '2.0.0'
 gem 'bcrypt-ruby', '3.0.1'
 gem 'faker', '1.0.1'
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -7,6 +7,14 @@ describe Micropost do
 
   subject { @micropost }
 
+  describe "accessible attributes" do
+    it "should not allow access to user_id" do
+      expect do
+        @micropost.update_attributes(user_id: user.id)
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end    
+  end
+
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
   it { should respond_to(:user) }
