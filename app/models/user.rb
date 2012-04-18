8b519cc175b24cab4c529c1ff19c631dea4e703c
Michael Hartl <michael@michaelhartl.com>
2012-04-17 19:01:43 -0700

    Add before_validation email downcase filter

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,13 @@ class User < ActiveRecord::Base
                                    dependent:   :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
 
+  before_validation { |user| user.email = email.downcase }
   before_save :create_remember_token
 
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
