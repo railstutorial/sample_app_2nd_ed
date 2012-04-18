bbe22eb82e08edabb6b24819ab49d15397be0aa8
Michael Hartl <michael@michaelhartl.com>
2012-04-17 18:30:11 -0700

    Remove uniqueness constraint for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,7 @@ class User < ActiveRecord::Base
 
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
