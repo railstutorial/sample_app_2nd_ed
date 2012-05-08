69c20ab275351e97d32b08878a7b8041af8040ea
Michael Hartl <michael@michaelhartl.com>
2012-05-07 17:03:24 -0700

    Slightly better valid email addresses

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -71,7 +71,7 @@ describe User do
 
   describe "when email format is valid" do
     it "should be valid" do
-      addresses = %w[user@foo.com A_USER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
       addresses.each do |valid_address|
         @user.email = valid_address
         @user.should be_valid
