a7d3f349ca24ae824fa84a4576cbeee479e74b41
Michael Hartl <michael@michaelhartl.com>
2012-05-10 17:13:29 -0700

    Ax unnecessary .png in gravatar_for

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
   def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
