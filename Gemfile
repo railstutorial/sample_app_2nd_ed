75f9ed68fc14ea0ac5bcdc2893c3039c82180687
Michael Hartl <michael@michaelhartl.com>
2012-05-14 15:39:17 -0700

    Eliminate duplicate test

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ end
 group :test do
   gem 'capybara', '1.1.2'
   gem 'factory_girl_rails', '1.4.0'
-  gem 'cucumber-rails', '1.2.1', require: false
+  gem 'cucumber-rails', '1.2.1', :require => false
   gem 'database_cleaner', '0.7.0'
   gem 'guard-spork', '0.3.2'  
   gem 'spork', '0.9.0'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -82,15 +82,6 @@ describe User do
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
-      user_with_same_email.save
-    end
-
-    it { should_not be_valid }
-  end
-
-  describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
