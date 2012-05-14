35e294d75203bf521d71afc6139af6bdbba938c2
Michael Hartl <michael@michaelhartl.com>
2012-05-14 13:47:28 -0700

    Ax some unnecessary session helper methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,7 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= user_from_remember_token
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
   def current_user?(user)
@@ -35,21 +35,10 @@ module SessionsHelper
 
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
-    clear_return_to
+    session.delete(:return_to)
   end
 
   def store_location
     session[:return_to] = request.fullpath
   end
-
-  private
-
-    def user_from_remember_token
-      remember_token = cookies[:remember_token]
-      User.find_by_remember_token(remember_token) unless remember_token.nil?
-    end
-    
-    def clear_return_to
-      session.delete(:return_to)
-    end
 end
