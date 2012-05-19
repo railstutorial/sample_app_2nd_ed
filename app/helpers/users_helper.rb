72be5679934d8e0d13db1091471f1c1594be2637
Michael Hartl <michael@michaelhartl.com>
2012-05-18 21:54:49 -0700

    Make Gravatars secure

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,8 @@ module UsersHelper
   def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar = 
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
