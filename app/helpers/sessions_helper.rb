82be4b5626406481dd0599d857f45585959f1c88
Michael Hartl <michael@michaelhartl.com>
2012-06-07 11:33:49 -0700

    Fix a bug in current_user assignment

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
-    current_user = user
+    self.current_user = user
   end
 
   def signed_in?
