f02407dd4cea753ca7ef17350ceb8360b8c90eef
Michael Hartl <michael@michaelhartl.com>
2012-10-10 14:33:23 -0700

    Fix a bug in the associated microposts test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -159,8 +159,9 @@ describe User do
     end
   
     it "should destroy associated microposts" do
-      microposts = @user.microposts
+      microposts = @user.microposts.dup
       @user.destroy
+      microposts.should_not be_empty
       microposts.each do |micropost|
         Micropost.find_by_id(micropost.id).should be_nil
       end
