97787789b6aa086e9114e70725b21f7be67aa5d8
Michael Hartl <michael@michaelhartl.com>
2013-02-09 17:04:39 -0800

    Add an #upcase in signin to test case-sensitivity

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,7 +29,7 @@ describe "Authentication" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email
+        fill_in "Email",    with: user.email.upcase
         fill_in "Password", with: user.password
         click_button "Sign in"
       end
@@ -37,7 +37,7 @@ describe "Authentication" do
       it { should have_selector('title', text: user.name) }
 
       it { should have_link('Users',    href: users_path) }
-      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Profile',  href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
