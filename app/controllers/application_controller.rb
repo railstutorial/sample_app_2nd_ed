17b5130881d868e1a439c223b8d0b6705a9b0a0e
Michael Hartl <michael@michaelhartl.com>
2013-03-16 13:36:06 -0700

    Add Jérôme Lambercy's CSRF fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+
+  # Force signout to prevent CSRF attacks
+  def handle_unverified_request
+    sign_out
+    super
+  end
 end
