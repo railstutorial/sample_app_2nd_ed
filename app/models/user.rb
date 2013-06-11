c325ef97c1ca163b43a5627e3646a4483401894e
Michael Hartl <michael@michaelhartl.com>
2013-06-10 18:17:54 -0700

    Improve method for suppressing password_digest error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,9 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  after_validation { self.errors.messages.delete(:password_digest) }
 
   def feed
     Micropost.from_users_followed_by(self)
